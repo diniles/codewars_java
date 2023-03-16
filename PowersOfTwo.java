d820b7a9feb599399ecc3039fc62b1bff0b0acc9
diniles <dzliashko@gmail.com>
2023-03-16 16:46:35 +0300

    Add: class PowersOfTwo

---
+++ PowersOfTwo.java
@@ -0,0 +1,34 @@
+/*
+https://www.codewars.com/kata/57a083a57cb1f31db7000028/train/java
+Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the
+exponent ranging from 0 to n ( inclusive ).
+
+Examples
+n = 0  ==> [1]        # [2^0]
+n = 1  ==> [1, 2]     # [2^0, 2^1]
+n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PowersOfTwo {
+    public static long[] powersOfTwo(int n) {
+        ArrayList<Long> powers = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            powers.add((long) Math.pow(2, i));
+        }
+        long[] result = new long[powers.size()];
+        for (int i = 0; i < powers.size(); i++) {
+            result[i]= powers.get(i);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(powersOfTwo(0)));
+        System.out.println(Arrays.toString(powersOfTwo(1)));
+        System.out.println(Arrays.toString(powersOfTwo(4)));
+
+    }
+}
