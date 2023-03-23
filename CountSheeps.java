65949b48d20afc90048a7f415842e63de98df70d
diniles <dzliashko@gmail.com>
2023-03-23 15:15:17 +0300

    Add: class CountSheeps

---
+++ CountSheeps.java
@@ -0,0 +1,24 @@
+/*
+https://www.codewars.com/kata/5b077ebdaf15be5c7f000077/train/java
+If you can't sleep, just count sheep!!
+
+Task:
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will
+always be valid, i.e. no negative integers.
+*/
+
+public class CountSheeps {
+    public static String countingSheep(int num) {
+        String result = "";
+        for (int i = 1; i <= num; i++) {
+            result += i + " sheep...";
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countingSheep(1));
+        System.out.println(countingSheep(2));
+        System.out.println(countingSheep(3));
+    }
+}
