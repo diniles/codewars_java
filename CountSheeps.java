2d528e8fb3579189e1abaabbdbade17663f4fea8
diniles <dzliashko@gmail.com>
2023-03-23 15:16:08 +0300

    Add: class CountSheeps best practices

--- CountSheeps.java
+++ CountSheeps.java
@@ -16,6 +16,16 @@ public class CountSheeps {
         return result;
     }
 
+/*  Best practices
+    public static String countingSheep(int num) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 1; i <= num; i++) {
+            stringBuilder.append(i).append(" sheep...");
+        }
+        return stringBuilder.toString();
+    }*/
+
+
     public static void main(String[] args) {
         System.out.println(countingSheep(1));
         System.out.println(countingSheep(2));
