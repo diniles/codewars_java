a3dfa4629c4f245d69d44cd7bc85cb4c2718c980
Dzmitry Liashko <dzliashko@gmail.com>
2023-04-06 18:58:30 +0300

    Add: class QuarterOf

--- QuarterOf.java
+++ QuarterOf.java
@@ -12,14 +12,13 @@ Constraint:
 
 public class QuarterOf {
     public static int quarterOf(int month) {
-        int quarter = switch (month) {
+        return switch (month) {
             case 1, 2, 3 -> 1;
             case 4, 5, 6 -> 2;
             case 7, 8, 9 -> 3;
             case 10, 11, 12 -> 4;
             default -> 0;
         };
-        return quarter;
     }
 
     public static void main(String[] args) {
