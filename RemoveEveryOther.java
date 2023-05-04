3defcc3477a6a6d6906fce320f4262585c2acce2
diniles <dzliashko@gmail.com>
2023-05-04 12:14:50 +0300

    Feat: add class RemoveEveryOther solved

--- RemoveEveryOther.java
+++ RemoveEveryOther.java
@@ -9,13 +9,26 @@ Example:
 None of the arrays will be empty, so you don't have to worry about that!
 */
 
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class RemoveEveryOther {
     public static Object[] removeEveryOther(Object[] arr) {
-        // happy coding
-        Object[] result = new Object[arr.length];
-        for (int i = 1, j = 0; i < result.length; i += 2, j++) {
-            result[j] = arr[i];
+        ArrayList<Object> list = new ArrayList<>();
+        for (int i = 0, j = 0; i < arr.length; i += 2, j++) {
+            list.add(arr[i]);
+        }
+        Object[] result = new Object[list.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(removeEveryOther(new Object[]{"Hello", "Goodbye", "Hello Again"})));
+        System.out.println(Arrays.toString(removeEveryOther(new Object[]{1, 2})));
+        System.out.println(Arrays.toString(removeEveryOther(new Object[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})));
+        System.out.println(Arrays.toString(removeEveryOther(new Object[]{"Goodbye"})));
     }
 }
