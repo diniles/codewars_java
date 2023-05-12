2adfae6e40c65d5370a66dc4c49e27ea5fa41521
Dzmitry Liashko <dzliashko@gmail.com>
2023-05-12 17:34:22 +0300

    Feat: add kata MinMaxInList

--- MinMaxInList/SolutionTest.java
+++ MinMaxInList/SolutionTest.java
@@ -4,10 +4,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.runners.JUnit4;
-
-// TODO: Replace examples and use TDD development by writing your own tests
-
 public class SolutionTest {
 
     private Kata kata = new Kata();
