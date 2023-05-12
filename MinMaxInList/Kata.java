226d3bf7c506bf0bd8aaff023d2e907264c0b81a
Dzmitry Liashko <dzliashko@gmail.com>
2023-05-12 17:33:50 +0300

    Feat: add kata MinMaxInList

---
+++ MinMaxInList/Kata.java
@@ -0,0 +1,33 @@
+/*
+https://www.codewars.com/kata/577a98a6ae28071780000989/train/java
+Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive
+a list of integers as input, and return the largest and lowest number in that list, respectively.
+
+Examples (Input -> Output)
+* [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+* [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+* [42, 54, 65, 87, 0]             -> min = 0, max = 87
+* [5]                             -> min = 5, max = 5
+Notes
+You may consider that there will not be any empty arrays/vectors.
+*/
+
+package MinMaxInList;
+
+public class Kata {
+    public int min(int[] list) {
+        int min = list[0];
+        for (int el : list) {
+            if (el < min) min = el;
+        }
+        return min;
+    }
+
+    public int max(int[] list) {
+        int max = list[0];
+        for (int el : list) {
+            if (el > max) max = el;
+        }
+        return max;
+    }
+}
--- Positive.java
+++ Positive.java
@@ -1,8 +1,8 @@
 public class Positive {
     public static int sum(int[] arr) {
         int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > 0) sum += arr[i];
+        for (int j : arr) {
+            if (j > 0) sum += j;
         }
         return sum;
     }
