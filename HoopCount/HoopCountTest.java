cc37ff994f39a3ad8c853a78901827ffe20a6f59
diniles <dzliashko@gmail.com>
2023-05-15 11:03:50 +0300

    Feat: kata HoopCount

---
+++ HoopCount/HoopCountTest.java
@@ -0,0 +1,13 @@
+package HoopCount;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HoopCountTest {
+    @Test
+    public void testHoopCount() {
+        assertEquals("Great, now move on to tricks", HelpAlex.hoopCount(11));
+        assertEquals("Keep at it until you get it", HelpAlex.hoopCount(7));
+    }
+}
