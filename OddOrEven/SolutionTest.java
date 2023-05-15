eab571ed2a86db6885b54ffebd06778c59afb4c4
diniles <dzliashko@gmail.com>
2023-05-15 11:08:59 +0300

    Feat: kata OddOrEven

---
+++ OddOrEven/SolutionTest.java
@@ -0,0 +1,14 @@
+package OddOrEven;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+    @Test
+    public void exampleTest() {
+        assertEquals("odd", OddOrEven.oddOrEven(new int[]{2, 5, 34, 6}));
+    }
+}
