da9a8b8b7761392d2886e691e313ce9c049d05ac
diniles <dzliashko@gmail.com>
2023-05-17 13:31:07 +0300

    Feat: kata grow best practice

--- grow/Kata.java
+++ grow/Kata.java
@@ -6,13 +6,18 @@ Given a non-empty array of integers, return the result of multiplying the values
 */
 
 package grow;
+//for best practice
+
+import java.util.stream.IntStream;
 
 public class Kata {
     public static int grow(int[] x) {
-        int result = 1;
-        for (int el : x) {
-            result *= el;
-        }
-        return result;
+//        int result = 1;
+//        for (int el : x) {
+//            result *= el;
+//        }
+//        return result;
+//        best practice
+        return IntStream.of(x).reduce(1, (a, b) -> a * b);
     }
 }
