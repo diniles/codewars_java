df4424cfd55e452949c245f4c91aa5c015c6a991
diniles <dzliashko@gmail.com>
2023-05-18 12:49:40 +0300

    Feat: kata CountBy

---
+++ countBy/CountBy.java
@@ -0,0 +1,26 @@
+/*
+https://www.codewars.com/kata/5513795bd3fafb56c200049e/train/java
+Create a function with two arguments that will return an array of the first n multiples of x.
+
+Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+Return the results as an array or list ( depending on language ).
+
+Examples
+countBy(1,10)  // should return  {1,2,3,4,5,6,7,8,9,10}
+countBy(2,5)  // should return {2,4,6,8,10}
+*/
+
+package countBy;
+
+public class CountBy {
+    public static int[] countBy(int x, int n) {
+        int[] array = new int[n];
+        int counter = x;
+        for (int i = 0; i < n; i++) {
+            array[i] = x;
+            x += counter;
+        }
+        return array;
+    }
+}
