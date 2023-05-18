4b00c31ff706d7081450de3feca6b2fad3936156
diniles <dzliashko@gmail.com>
2023-05-18 12:41:47 +0300

    Feat: kata StrCount best practice

--- strCount/StrCount.java
+++ strCount/StrCount.java
@@ -20,10 +20,13 @@ package strCount;
 
 public class StrCount {
     public static int strCount(String str, char letter) {
-        int sum = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == letter) sum++;
-        }
-        return sum;
+//        int sum = 0;
+//        for (int i = 0; i < str.length(); i++) {
+//            if (str.charAt(i) == letter) sum++;
+//        }
+//        return sum;
+
+//        best practice
+        return (int) str.chars().filter(x -> x == letter).count();
     }
 }
