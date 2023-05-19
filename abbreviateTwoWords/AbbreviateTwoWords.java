96699c69999d76dfd82f2aa636de1a6ec341b980
diniles <dzliashko@gmail.com>
2023-05-19 13:54:59 +0300

    Feat: kata AbbreviateTwoWords

---
+++ abbreviateTwoWords/AbbreviateTwoWords.java
@@ -0,0 +1,21 @@
+/*
+https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3/train/java
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+*/
+
+package abbreviateTwoWords;
+
+public class AbbreviateTwoWords {
+    public static String abbrevName(String name) {
+        String result = name.charAt(0) + "." + name.charAt(name.indexOf(' ') + 1);
+        return result.toUpperCase();
+    }
+}
