8e056c8969e01031624331f7ae36ce7653df023a
diniles <dzliashko@gmail.com>
2023-06-05 14:47:29 +0300

    Feat: package YesOrNo

--- yesorno/YesOrNo.java
+++ yesorno/YesOrNo.java
@@ -1,7 +1,12 @@
+/*
+https://www.codewars.com/kata/53369039d7ab3ac506000467/train/java
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+
 package yesorno;
 
 public class YesOrNo {
     public static String boolToWord(boolean b) {
-        //TODO
+        return b ? "Yes" : "No";
     }
 }
---
+++ yesorno/YesOrNoTest.java
@@ -0,0 +1,13 @@
+package yesorno;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class YesOrNoTest {
+    @Test
+    public void testBoolToWord() {
+        assertEquals(YesOrNo.boolToWord(true), "Yes");
+        assertEquals(YesOrNo.boolToWord(false), "No");
+    }
+}
