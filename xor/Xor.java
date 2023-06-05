d7e74d58469df7627179981a36c576b773298448
diniles <dzliashko@gmail.com>
2023-06-05 14:42:42 +0300

    Feat: package xor best practice

--- xor/Xor.java
+++ xor/Xor.java
@@ -2,7 +2,8 @@ package xor;
 
 public class Xor {
     public static boolean xor(boolean a, boolean b) {
-        //your code here:
-        return (a || b) && (a != b);
+//        best practice
+        return a != b;
+//        return (a || b) && (a != b);
     }
 }
