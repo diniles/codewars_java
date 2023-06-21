a50c749b540dfcaf71693ca6a733313d3a20b629
diniles <dzliashko@gmail.com>
2023-06-21 12:49:37 +0300

    Feat: package DoubleInteger

---
+++ doubleInteger/DoubleIntegerTest.java
@@ -0,0 +1,13 @@
+package doubleInteger;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class DoubleIntegerTest {
+    @Test
+    public final void test_all() {
+        assertEquals(4, DoubleInteger.doubleInteger(2)); // Test cases here!
+    }
+}
