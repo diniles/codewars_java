0d204c0f0d3d0526b24299e2e9d908bf67696596
diniles <dzliashko@gmail.com>
2023-06-22 13:06:05 +0300

    Refactor: package IsEven

--- IsEven/IsEvenTest.java
+++ IsEven/IsEvenTest.java
@@ -2,16 +2,16 @@ package IsEven;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class IsEvenTest {
 
     @Test
     public void test() {
-        assertEquals(true, IsEven.isEven(0));
-        assertEquals(false, IsEven.isEven(0.5));
-        assertEquals(false, IsEven.isEven(1));
-        assertEquals(true, IsEven.isEven(2));
-        assertEquals(true, IsEven.isEven(-4));
+        assertTrue(IsEven.isEven(0));
+        assertFalse(IsEven.isEven(0.5));
+        assertFalse(IsEven.isEven(1));
+        assertTrue(IsEven.isEven(2));
+        assertTrue(IsEven.isEven(-4));
     }
 }
