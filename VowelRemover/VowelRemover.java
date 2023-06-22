58b2533697057b73f992910e02a6e1dccca81e6b
diniles <dzliashko@gmail.com>
2023-06-22 12:18:37 +0300

    Feat: package VowelRemover

---
+++ VowelRemover/VowelRemover.java
@@ -0,0 +1,19 @@
+/*
+https://www.codewars.com/kata/5547929140907378f9000039/train/java
+Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+Examples
+"hello"     -->  "hll"
+"codewars"  -->  "cdwrs"
+"goodbye"   -->  "gdby"
+"HELLO"     -->  "HELLO"
+*/
+
+package VowelRemover;
+
+public class VowelRemover {
+    public static String shortcut(String input) {
+        String vowelsLowerCase = "[aeiou]";
+        return input.replaceAll(vowelsLowerCase, "");
+    }
+}
---
+++ VowelRemover/VowelRemoverTest.java
@@ -0,0 +1,15 @@
+package VowelRemover;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class VowelRemoverTest {
+    @Test
+    void test1() {
+        assertEquals("hll", VowelRemover.shortcut("hello"));
+        assertEquals("hw r y tdy?", VowelRemover.shortcut("how are you today?"));
+        assertEquals("cmpln", VowelRemover.shortcut("complain"));
+        assertEquals("nvr", VowelRemover.shortcut("never"));
+    }
+}
