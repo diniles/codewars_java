aba6d825a69dd5b6319cb7013880ff728a130c51
diniles <dzliashko@gmail.com>
2023-06-23 10:12:37 +0300

    Refactor: package unique task FindUniqNumber

--- unique/FindUniqNumberTest.java
+++ unique/FindUniqNumberTest.java
@@ -5,10 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FindUniqNumberTest {
-    private double precision = 0.0000000000001;
 
     @Test
     public void sampleTestCases() {
+        double precision = 0.0000000000001;
         assertEquals(1.0, FindUniqNumber.findUniq(new double[]{0, 1, 0}), precision);
         assertEquals(2.0, FindUniqNumber.findUniq(new double[]{1, 1, 1, 2, 1, 1}), precision);
     }
