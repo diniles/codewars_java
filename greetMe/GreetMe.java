3b2261bc6269be08335c388e4e41ec32c69f3122
diniles <dzliashko@gmail.com>
2023-07-25 10:43:20 +0300

    Feat: package greetMe solved

--- greetMe/GreetMe.java
+++ greetMe/GreetMe.java
@@ -1,3 +1,13 @@
+/*
+https://www.codewars.com/kata/535474308bb336c9980006f2/train/java
+Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+Example:
+
+"riley" --> "Hello Riley!"
+"JACK"  --> "Hello Jack!"
+*/
+
 package greetMe;
 
 public class GreetMe {
