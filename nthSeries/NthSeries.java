2eaa1da55d9bb548d313ab8a647eeb682f029db1
Dzmitry Liashko <dzliashko@gmail.com>
2023-08-05 18:07:16 +0300

    Feat: add kata nthSeries

---
+++ nthSeries/NthSeries.java
@@ -0,0 +1,37 @@
+/*
+https://www.codewars.com/kata/555eded1ad94b00403000071/train/java
+Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+Rules:
+You need to round the answer to 2 decimal places and return it as String.
+
+If the given value is 0 then it should return 0.00
+
+You will only be given Natural Numbers as arguments.
+
+Examples:(Input --> Output)
+1 --> 1 --> "1.00"
+2 --> 1 + 1/4 --> "1.25"
+5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+*/
+
+package nthSeries;
+
+public class NthSeries {
+    public static String seriesSum(int n) {
+        String result = "";
+        double sum = 1;
+        if (n == 0) {
+            result = "0.00";
+        } else if (n == 1) {
+            result = "1.00";
+        } else {
+            for (int i = 4; i < n * 3; i = i + 3) {
+                sum += (double) 1 / i;
+            }
+            result += String.format("%.2f", sum);
+        }
+        return result;
+    }
+}
---
+++ nthSeries/NthSeriesTest.java
@@ -0,0 +1,22 @@
+package nthSeries;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NthSeriesTest {
+    @Test
+    public void test1() {
+        assertEquals("1.57", NthSeries.seriesSum(5));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals("1.77", NthSeries.seriesSum(9));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals("1.94", NthSeries.seriesSum(15));
+    }
+}
